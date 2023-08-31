29bb3fa9c23e5624640b50a4ed7585209d60014c
BenjaminAcuff <113147871+BenjaminAcuff@users.noreply.github.com>
2023-08-31 19:26:43 -0400

    Add files via upload

--- detail.php
+++ detail.php
@@ -5,6 +5,134 @@ $currentPage = 0;
 $resumes = [
 	[
 		'Title' => 
+			'Ben Acuff\'s Resume',
+		
+			'Header' => 
+		[
+				'Name' => 'Ben Acuff',
+				'Headshot' => 'assets/images/profile.jpg',
+				'Position' => 'Cybersecurity Analyst',
+				'Email' => 'acuffb1@mymail.nku.edu',
+				'Phone' => '(513) 371-4289',
+				'LinkedIn' => 'https://www.linkedin.com/in/benjaminacuff',
+				'GitHub' => 'https://www.github.com/benjaminacuff',
+				'Website' =>  'https://www.benacuff.com',
+		],
+		
+		'Summary' =>
+			'I am currently am a junior attending Northern Kentucky University. My major is cybersecurity, while my minor is computer science.',
+		'WorkExperience',
+		
+			'WorkExperience' =>
+		[
+			[
+				'Job_Title' => "Web Developer",
+				'Job_Location' => "Northern Kentucky University",
+				'Job_StartFinish' => "August 2023 - Present",
+				'Job_Descr' => "Work on websites for Northern Kentucky University.",
+				'Job_AchDescr' => "Here are some of my achievements at my job below.",
+				'Job_Ach_1' => "96.8% customer satisfaction rate.",
+				'Job_Ach_2' => "Employee of the month: August 2023.",
+				'Job_Ach_3' => "NSA Certified as of August 2023.",
+				'Job_Ach_4' => "8,900+ bugs fixed.",
+				'Job_TechUsed_1' => "Python",
+				'Job_TechUsed_2' => "jQuery",
+				'Job_TechUsed_3' => "Webpack",
+				'Job_TechUsed_4' => "PostgressSQL",
+				'Job_TechUsed_5' => "HTML / CSS",
+			],
+			[
+				'Job_Title' => "Junior Software Developer",
+				'Job_Location' => "Amazon",
+				'Job_StartFinish' => "May 2022 - August 2023",
+				'Job_Descr' => "Develop various softwares for Amazon.",
+				'Job_AchDescr' => "Achievements:",
+				'Job_Ach_1' => "Artificial Intelligence Project",
+				'Job_Ach_2' => "Oracle SQL Certified",
+				'Job_Ach_3' => "Debugging Award",
+				'Job_Ach_4' => "Employee of the month: April 2022 and October 2022",
+				'Job_TechUsed_1' => "Redux",
+				'Job_TechUsed_2' => "Java",
+				'Job_TechUsed_3' => "Webpack",
+				'Job_TechUsed_4' => "HTML",
+				'Job_TechUsed_5' => "SASS",
+			]
+		],
+		'ProgressBarSkills' =>
+		[
+			[
+			'Skill_1_Name' => "Java",
+			'Skill_1_Progress' => "91",
+			],
+			[
+			'Skill_2_Name' => "HTML/CSS/JavaScript",
+			'Skill_2_Progress' => "98",
+			],
+			[
+			'Skill_3_Name' => "Oracle SQL",
+			'Skill_3_Progress' => "89",
+			],
+		],
+		'OtherSkills' =>
+		[
+			'Other_Skill_1' => "Django",
+			'Other_Skill_2' => "Leadership",
+			'Other_Skill_3' => "Require.js",
+			'Other_Skill_4' => "HTML",
+			'Other_Skill_5' => "CSS",
+			'Other_Skill_6' => "Teamwork",
+			'Other_Skill_7' => "Communication",
+			'Other_Skill_8' => "Network",
+		],
+		'Education' =>
+		[
+			[
+			'Ed_Degree' => "BS CyberSecurity",
+			'Ed_Location' => "Northern Kentucky University",
+			'Ed_Time' => "2021 - Present",
+			],
+		],
+		'Awards' =>
+		[
+			[
+			'Award_1_Name' => "Governors Scholar",
+			'Award_1_Descr' => "Scholarship awarded to distinguished students in the state of Kentucky.",
+			],
+			[
+			'Award_2_Name' => "Congressional Award",
+			'Award_2_Descr' => "Award given to volunteers in Kentucky with over 300 clocked hours.",
+			],
+		],
+		'Languages' =>
+		[
+			'Lang_1' => "Java",
+			'Lang_2' => "C++",
+			'Lang_3' => "PHP",
+		],
+		'Interests' =>
+		[
+			'Interest_1' => "Weightlifting",
+			'Interest_2' => "Running",
+			'Interest_3' => "Pokemon",
+		],
+		'Projects' =>
+		[
+			[
+				'Project_1_IMG' => "./assets/images/project1.jpg",
+				'Project_1_Title' => "Artificial Intelligence",
+				'Project_1_Desc' => "What is artifical intelligence?",
+				'Project_1_Link' => "https://docs.google.com/document/d/1JaqQTobGlJybkl7kMct9pRElvS3MKo89zFCyXw-GsMs/edit?usp=sharing",
+			],
+			[
+				'Project_1_IMG' => "./assets/images/project2.jpg",
+				'Project_1_Title' => "Machine Learning",
+				'Project_1_Desc' => "What is machine learning?",
+				'Project_1_Link' => "https://docs.google.com/document/d/1AvzpVbB8Icy7B_wtfKFRNJwUk-qZnsvp2iOMIwybh1I/edit?usp=sharing",
+			]
+		]
+	],
+	[
+		'Title' => 
 			'Ben Molloy\'s Resume',
 		
 			'Header' => 
@@ -116,6 +244,7 @@ $resumes = [
 		],
 		'Projects' =>
 		[
+			'Project' =>
 			[
 				'Project_1_IMG' => "./assets/images/project1.jpg",
 				'Project_1_Title' => "CodeOptimize: Performance Tuning Project",
@@ -123,12 +252,6 @@ $resumes = [
 				'Project_1_Link' => "https://www.codeoptimize-example.com",
 			]
 		]
-	],
-	[
-
-	],
-	[
-
 	]
 ]
 
@@ -166,7 +289,7 @@ $resumes = [
 		    <header class="resume-header pt-4 pt-md-0">
 			    <div class="row">
 				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-				        <img class="picture" src="<?=$resumes[$currentPage]['Header']['Headshot'];?>" alt="">
+				        <img class="picture" src="assets/images/profile.jpg" alt="">
 				    </div><!--//col-->
 				    <div class="col">
 					    <div class="row p-4 justify-content-center justify-content-md-between">
@@ -174,15 +297,15 @@ $resumes = [
 							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase"><?=$resumes[$currentPage]['Header']['Name'];?></h1>
 							    <div class="title mb-3"><?=$resumes[$currentPage]['Header']['Position'];?></div>
 							    <ul class="list-unstyled">
-								    <li class="mb-2"><a class="text-link" href="<?=$resumes[$currentPage]['Header']['Email'];?>"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i><?=$resumes[$currentPage]['Header']['Email'];?></li>
-								    <li><a class="text-link" href="<?=$resumes[$currentPage]['Header']['Phone'];?>"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i><?=$resumes[$currentPage]['Header']['Phone'];?></a></li>
+								    <li class="mb-2"><a class="text-link" href="#"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i><?=$resumes[$currentPage]['Header']['Email'];?></li>
+								    <li><a class="text-link" href="#"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i><?=$resumes[$currentPage]['Header']['Phone'];?></a></li>
 							    </ul>
 						    </div><!--//primary-info-->
 						    <div class="secondary-info col-auto mt-2">
 							    <ul class="resume-social list-unstyled">
-					                <li class="mb-3"><a class="text-link" href="<?=$resumes[$currentPage]['Header']['LinkedIn'];?>"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span><?=$resumes[$currentPage]['Header']['LinkedIn'];?></a></li>
-					                <li class="mb-3"><a class="text-link" href="<?=$resumes[$currentPage]['Header']['GitHub'];?>"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span><?=$resumes[$currentPage]['Header']['GitHub'];?></a></li>
-					                <li><a class="text-link" href="<?=$resumes[$currentPage]['Header']['Website'];?>"><span class="fa-container text-center me-2"><i class="fas fa-globe"></i></span><?=$resumes[$currentPage]['Header']['Website'];?></a></li>
+					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span><?=$resumes[$currentPage]['Header']['LinkedIn'];?></a></li>
+					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span><?=$resumes[$currentPage]['Header']['GitHub'];?></a></li>
+					                <li><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fas fa-globe"></i></span><?=$resumes[$currentPage]['Header']['Website'];?></a></li>
 							    </ul>
 						    </div><!--//secondary-info-->
 					    </div><!--//row-->
@@ -278,14 +401,15 @@ $resumes = [
 						        <div class="resume-skill-item">
 						            <h4 class="resume-skills-cat font-weight-bold">Others</h4>
 						            <ul class="list-inline">
-							            <li class="list-inline-item"><span class="badge badge-light"><?=$resumes[$currentPage]['OtherSkills']['Other_Skill_1'];?></span></li>
-							            <li class="list-inline-item"><span class="badge badge-light"><?=$resumes[$currentPage]['OtherSkills']['Other_Skill_2'];?></span></li>
-							            <li class="list-inline-item"><span class="badge badge-light"><?=$resumes[$currentPage]['OtherSkills']['Other_Skill_3'];?></span></li>
-							            <li class="list-inline-item"><span class="badge badge-light"><?=$resumes[$currentPage]['OtherSkills']['Other_Skill_4'];?></span></li>
-							            <li class="list-inline-item"><span class="badge badge-light"><?=$resumes[$currentPage]['OtherSkills']['Other_Skill_5'];?></span></li>
-							            <li class="list-inline-item"><span class="badge badge-light"><?=$resumes[$currentPage]['OtherSkills']['Other_Skill_6'];?></span></li>
-							            <li class="list-inline-item"><span class="badge badge-light"><?=$resumes[$currentPage]['OtherSkills']['Other_Skill_7'];?></span></li>
-							            <li class="list-inline-item"><span class="badge badge-light"><?=$resumes[$currentPage]['OtherSkills']['Other_Skill_8'];?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">DevOps</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Code Review</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Git</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Unit Testing</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Wireframing</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Sketch</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Balsamiq</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">WordPress</span></li>
+							            <li class="list-inline-item"><span class="badge badge-light">Shopify</span></li>
 						            </ul>
 						        </div><!--//resume-skill-item-->
 						    </div><!--resume-section-content-->
@@ -295,9 +419,14 @@ $resumes = [
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled">
 								    <li class="mb-2">
-								        <div class="resume-degree font-weight-bold"><?=$resumes[$currentPage]['Education']['Ed_Degree'];?></div>
-								        <div class="resume-degree-org"><?=$resumes[$currentPage]['Education']['Ed_Location'];?></div>
-								        <div class="resume-degree-time"><?=$resumes[$currentPage]['Education']['Ed_Time'];?></div>
+								        <div class="resume-degree font-weight-bold">MSc in Computer Science</div>
+								        <div class="resume-degree-org">University College London</div>
+								        <div class="resume-degree-time">2013 - 2014</div>
+								    </li>
+								    <li>
+								        <div class="resume-degree font-weight-bold">BSc Maths and Physics</div>
+								        <div class="resume-degree-org">Imperial College London</div>
+								        <div class="resume-degree-time">2010 - 2013</div>
 								    </li>
 							    </ul>
 						    </div>
@@ -308,13 +437,13 @@ $resumes = [
 							    <ul class="list-unstyled resume-awards-list">
 								    <li class="mb-2 ps-4 position-relative">
 								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
-								        <div class="resume-award-name"><?=$resumes[$currentPage]['Awards']['Award_1_Name'];?></div>
-								        <div class="resume-award-desc"><?=$resumes[$currentPage]['Awards']['Award_1_Descr'];?></div>
+								        <div class="resume-award-name">Award Name Lorem</div>
+								        <div class="resume-award-desc">Award desc goes here, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.</div>
 								    </li>
 								    <li class="mb-0 ps-4 position-relative">
 								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
-								        <div class="resume-award-name"><?=$resumes[$currentPage]['Awards']['Award_2_Name'];?></div>
-								        <div class="resume-award-desc"><?=$resumes[$currentPage]['Awards']['Award_2_Descr'];?></div>
+								        <div class="resume-award-name">Award Name Ipsum</div>
+								        <div class="resume-award-desc">Award desc goes here, ultricies nec, pellentesque.</div>
 								    </li>
 							    </ul>
 						    </div>
@@ -323,9 +452,9 @@ $resumes = [
 						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Languages</h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled resume-lang-list">
-								    <li class="mb-2"><span class="resume-lang-name font-weight-bold"><?=$resumes[$currentPage]['Languages']['Lang_1'];?></span> <small class="text-muted font-weight-normal">(Native)</small></li>
-								    <li class="mb-2 align-middle"><span class="resume-lang-name font-weight-bold"><?=$resumes[$currentPage]['Languages']['Lang_2'];?></span> <small class="text-muted font-weight-normal">(Professional)</small></li>
-								    <li><span class="resume-lang-name font-weight-bold"><?=$resumes[$currentPage]['Languages']['Lang_3'];?></span> <small class="text-muted font-weight-normal">(Professional)</small></li>
+								    <li class="mb-2"><span class="resume-lang-name font-weight-bold">English</span> <small class="text-muted font-weight-normal">(Native)</small></li>
+								    <li class="mb-2 align-middle"><span class="resume-lang-name font-weight-bold">French</span> <small class="text-muted font-weight-normal">(Professional)</small></li>
+								    <li><span class="resume-lang-name font-weight-bold">Spanish</span> <small class="text-muted font-weight-normal">(Professional)</small></li>
 							    </ul>
 						    </div>
 					    </section><!--//language-section-->
@@ -333,9 +462,9 @@ $resumes = [
 						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Interests</h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled">
-								    <li class="mb-1"><?=$resumes[$currentPage]['Interests']['Interest_1'];?></li>
-								    <li class="mb-1"><?=$resumes[$currentPage]['Interests']['Interest_2'];?></li>
-								    <li class="mb-1"><?=$resumes[$currentPage]['Interests']['Interest_3'];?></li>
+								    <li class="mb-1">Climbing</li>
+								    <li class="mb-1">Snowboarding</li>
+								    <li class="mb-1">Cooking</li>
 							    </ul>
 						    </div>
 					    </section><!--//interests-section-->
@@ -347,11 +476,31 @@ $resumes = [
 					<div class="row mt-4">
 						<div class="col-md-4">
 							<div class="card">
-								<img src="<?=$resumes[$currentPage]['Projects'][0]['Project_1_IMG'];?>" alt="Project 1" class="card-img-top">
+								<img src="path-to-project-image1.jpg" alt="Project 1" class="card-img-top">
 								<div class="card-body">
-									<h5 class="card-title"><?=$resumes[$currentPage]['Projects'][0]['Project_1_Title'];?></h5>
-									<p class="card-text"><?=$resumes[$currentPage]['Projects'][0]['Project_1_Desc'];?></p>
-									<a href="btn btn-outline-primary" href="<?=$resumes[$currentPage]['Projects'][0]['Project_1_Link'];?>"><?=$resumes[$currentPage]['Projects'][0]['Project_1_Link'];?></a>
+									<h5 class="card-title">Project 1</h5>
+									<p class="card-text">Brief description of Project 1.</p>
+									<a href="btn btn-outline-primary" href="#">Go to link</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image2.jpg" alt="Project 2" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title">Project 2</h5>
+									<p class="card-text">Brief description of Project 2.</p>
+									<a href="btn btn-outline-primary" href="#">Go to link</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image3.jpg" alt="Project 3" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title">Project 3</h5>
+									<p class="card-text">Brief description of Project 3.</p>
+									<a href="btn btn-outline-primary" href="#">Go to link</a>
 								</div>
 							</div>
 						</div>
