49d41ba7a910e4e22fe471952d4e4eaec5d02bf0
BCoop123 <brandoncooper859@gmail.com>
2023-08-31 19:52:56 -0400

    push

--- detail.php
+++ detail.php
@@ -10,7 +10,7 @@ $resumes = [
 			'Header' => 
 		[
 				'Name' => 'Ben Molloy',
-				'Headshot' => 'assets/images/profile.jpg',
+				'Headshot' => 'assets/images/BenMProfile.jpg',
 				'Position' => 'Senior Network Analyst Manager',
 				'Email' => 'btmolloy2@gmail.com',
 				'Phone' => '(513) 374-8989',
@@ -55,7 +55,39 @@ $resumes = [
 				'Job_TechUsed_3' => "Metasploit",
 				'Job_TechUsed_4' => "Python",
 				'Job_TechUsed_5' => "Bash",
-			]
+			],
+			[
+				'Job_Title' => "Cybersecurity Analyst Intern",
+				'Job_Location' => "CyberGuard Solutions",
+				'Job_StartFinish' => "May 2023 - August 2023",
+				'Job_Descr' => "Assisted in conducting vulnerability assessments, analyzing security logs, and contributing to incident response efforts, while gaining hands-on experience in identifying and mitigating potential cybersecurity risks within a dynamic organizational environment.",
+				'Job_AchDescr' => "Was Great in overall performance.",
+				'Job_Ach_1' => "Faculty Replacement Project",
+				'Job_Ach_2' => "Office Organization Project",
+				'Job_Ach_3' => "File System Organization",
+				'Job_Ach_4' => "Stuctured Windows AD configuration",
+				'Job_TechUsed_1' => "SIEM",
+				'Job_TechUsed_2' => "IDS/IPS",
+				'Job_TechUsed_3' => "Firewall Management",
+				'Job_TechUsed_4' => "Threat Intelligence Platforms",
+				'Job_TechUsed_5' => "Endpoint Security Solutions",
+			],
+			[
+				'Job_Title' => "Information Security Intern",
+				'Job_Location' => "SecureNet Systems",
+				'Job_StartFinish' => "August 2023 - Current",
+				'Job_Descr' => "Contributed to security audits, participated in policy review and development, and gained exposure to vulnerability assessments, helping to fortify the company's information security framework while expanding practical knowledge of cybersecurity practices.",
+				'Job_AchDescr' => "Implemented automated scripts that streamlined security tasks, increasing operational efficiency by reducing manual effort and saving approximately 10 hours per week.",
+				'Job_Ach_1' => "Vulnerability Scanning Tools",
+				'Job_Ach_2' => "Security Policy Management Platforms",
+				'Job_Ach_3' => "Encryption Technologies",
+				'Job_Ach_4' => "Access Control Systems",
+				'Job_TechUsed_1' => "Wireshark",
+				'Job_TechUsed_2' => "Nmap",
+				'Job_TechUsed_3' => "Metasploit",
+				'Job_TechUsed_4' => "Python",
+				'Job_TechUsed_5' => "Bash",
+			],
 		],
 		'ProgressBarSkills' =>
 		[
@@ -70,6 +102,10 @@ $resumes = [
 			[
 			'SkillName' => "Node.js",
 			'SkillProgress' => "92",
+			],
+			[
+			'SkillName' => "PHP",
+			'SkillProgress' => "90",
 			],
 		],
 		'OtherSkills' =>
@@ -117,15 +153,152 @@ $resumes = [
 		'Projects' =>
 		[
 			[
-				'Project_1_IMG' => "./assets/images/project1.jpg",
+				'Project_1_IMG' => "./assets/images/BenMProject1.jpg",
 				'Project_1_Title' => "CodeOptimize: Performance Tuning Project",
 				'Project_1_Desc' => "The CodeOptimize project revolved around fine-tuning code for optimal execution, implementing efficiency-driven techniques, and profiling to enhance software speed and resource utilization.",
 				'Project_1_Link' => "https://www.codeoptimize-example.com",
+			],
+			[
+				'Project_1_IMG' => "./assets/images/BenMProject2.jpg",
+				'Project_1_Title' => "CodeCraft Enhancement Project",
+				'Project_1_Desc' => "The CodeCraft Enhancement Project focused on refining and optimizing existing codebase, introducing automation solutions to streamline processes and enhance overall software performance.",
+				'Project_1_Link' => "https://www.codecraft-enhancement.com",
+			],
+			[
+				'Project_1_IMG' => "./assets/images/BenMProject3.jpg",
+				'Project_1_Title' => "Project ServerForge",
+				'Project_1_Desc' => "Project ServerForge involved the meticulous design, assembly, and configuration of a robust server infrastructure, ensuring optimal performance, scalability, and reliability to meet the organization's data processing needs.",
+				'Project_1_Link' => "https://www.serverforge-example.com",
+			],			
+		]
+		],
+	[
+		'Title' => 
+			'Ben Acuff\'s Resume',
+		
+			'Header' => 
+		[
+				'Name' => 'Ben Acuff',
+				'Headshot' => 'assets/images/BenAProfile.jpg',
+				'Position' => 'Cybersecurity Analyst',
+				'Email' => 'acuffb1@mymail.nku.edu',
+				'Phone' => '(513) 371-4289',
+				'LinkedIn' => 'https://www.linkedin.com/in/benjaminacuff',
+				'GitHub' => 'https://www.github.com/benjaminacuff',
+				'Website' =>  'https://www.benacuff.com',
+		],
+		
+		'Summary' =>
+			'I am currently am a junior attending Northern Kentucky University. My major is cybersecurity, while my minor is computer science.',
+		'WorkExperience',
+		
+			'WorkExperience' =>
+		[
+			[
+				'Job_Title' => "Web Developer",
+				'Job_Location' => "Northern Kentucky University",
+				'Job_StartFinish' => "August 2023 - Present",
+				'Job_Descr' => "Work on websites for Northern Kentucky University.",
+				'Job_AchDescr' => "Here are some of my achievements at my job below.",
+				'Job_Ach_1' => "96.8% customer satisfaction rate.",
+				'Job_Ach_2' => "Employee of the month: August 2023.",
+				'Job_Ach_3' => "NSA Certified as of August 2023.",
+				'Job_Ach_4' => "8,900+ bugs fixed.",
+				'Job_TechUsed_1' => "Python",
+				'Job_TechUsed_2' => "jQuery",
+				'Job_TechUsed_3' => "Webpack",
+				'Job_TechUsed_4' => "PostgressSQL",
+				'Job_TechUsed_5' => "HTML / CSS",
+			],
+			[
+				'Job_Title' => "Junior Software Developer",
+				'Job_Location' => "Amazon",
+				'Job_StartFinish' => "May 2022 - August 2023",
+				'Job_Descr' => "Develop various softwares for Amazon.",
+				'Job_AchDescr' => "Achievements:",
+				'Job_Ach_1' => "Artificial Intelligence Project",
+				'Job_Ach_2' => "Oracle SQL Certified",
+				'Job_Ach_3' => "Debugging Award",
+				'Job_Ach_4' => "Employee of the month: April 2022 and October 2022",
+				'Job_TechUsed_1' => "Redux",
+				'Job_TechUsed_2' => "Java",
+				'Job_TechUsed_3' => "Webpack",
+				'Job_TechUsed_4' => "HTML",
+				'Job_TechUsed_5' => "SASS",
+			]
+		],
+		'ProgressBarSkills' =>
+		[
+			[
+			'SkillName' => "Java",
+			'SkillProgress' => "91",
+			],
+			[
+			'SkillName' => "HTML/CSS/JavaScript",
+			'SkillProgress' => "98",
+			],
+			[
+			'SkillName' => "Oracle SQL",
+			'SkillProgress' => "89",
+			],
+		],
+		'OtherSkills' =>
+		[
+			'Other_Skill_1' => "Django",
+			'Other_Skill_2' => "Leadership",
+			'Other_Skill_3' => "Require.js",
+			'Other_Skill_4' => "HTML",
+			'Other_Skill_5' => "CSS",
+			'Other_Skill_6' => "Teamwork",
+			'Other_Skill_7' => "Communication",
+			'Other_Skill_8' => "Network",
+		],
+		'Education' =>
+		[
+			[
+			'Ed_Degree' => "BS CyberSecurity",
+			'Ed_Location' => "Northern Kentucky University",
+			'Ed_Time' => "2021 - Present",
+			],
+		],
+		'Awards' =>
+		[
+			[
+			'AwardName' => "Governors Scholar",
+			'AwardDesc' => "Scholarship awarded to distinguished students in the state of Kentucky.",
+			],
+			[
+			'AwardName' => "Congressional Award",
+			'AwardDesc' => "Award given to volunteers in Kentucky with over 300 clocked hours.",
+			],
+		],
+		'Languages' =>
+		[
+			'Lang_1' => "Java",
+			'Lang_2' => "C++",
+			'Lang_3' => "PHP",
+		],
+		'Interests' =>
+		[
+			'Interest_1' => "Weightlifting",
+			'Interest_2' => "Running",
+			'Interest_3' => "Pokemon",
+		],
+		'Projects' =>
+		[
+			[
+				'Project_1_IMG' => "./assets/images/ArtificialIntelligence.jpg",
+				'Project_1_Title' => "Artificial Intelligence",
+				'Project_1_Desc' => "What is artifical intelligence?",
+				'Project_1_Link' => "https://docs.google.com/document/d/1JaqQTobGlJybkl7kMct9pRElvS3MKo89zFCyXw-GsMs/edit?usp=sharing",
+			],
+			[
+				'Project_1_IMG' => "./assets/images/MachineLearning.jpg",
+				'Project_1_Title' => "Machine Learning",
+				'Project_1_Desc' => "What is machine learning?",
+				'Project_1_Link' => "https://docs.google.com/document/d/1AvzpVbB8Icy7B_wtfKFRNJwUk-qZnsvp2iOMIwybh1I/edit?usp=sharing",
 			]
 		]
-	],
-	[
-
 	],
 	[
 
