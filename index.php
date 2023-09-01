eeae8add2e2260cd48815ca2d0586cc3f3381e9a
Ben <btmolloy2@gmail.com>
2023-09-01 19:39:31 -0400

    Index Stuff

--- index.php
+++ index.php
@@ -1,11 +1,35 @@
 <?php
-
+	$members = [
+		'pageTitle' => 'Team BBB',
+		'pageFooter' => 'Ben Molloy, Brandon Cooper, Ben Acuff',
+		'memberInfo' => 
+		[
+			[
+				'name' => 'Brandon Cooper',
+				'headshot' => 'assets/images/BrandonCProfile.jpg',
+				'role' => 'Wizard Coder',
+				'link' => '2',
+			],
+			[
+				'name' => 'Ben Molloy',
+				'headshot' => 'assets/images/BenMProfile.jpg',
+				'role' => 'Mystical Coder',
+				'link' => '0',
+			],
+			[
+				'name' => 'Ben Acuff',
+				'headshot' => 'assets/images/BenAProfile.jpg',
+				'role' => 'Godly Coder',
+				'link' => '1',
+			],
+		],
+	]
 
 ?>
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-    <title>Our amazing team</title>
+    <title><?=$members['pageTitle'];?></title>
     
     <!-- Meta -->
     <meta charset="utf-8">
@@ -23,81 +47,43 @@
        
     <!-- Theme CSS -->  
     <link id="theme-style" rel="stylesheet" href="assets/css/pillar-1.css">
-
-
 </head> 
 
 <body>
     <article class="resume-wrapper text-center position-relative">
 	    <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
-			<h1 class="py-4 text-center">OUR AMAZING TEAM</h1>
-		    <header class="resume-header pt-4 pt-md-0">
-			    <div class="row">
-				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-				        <img class="picture" src="assets/images/profile.jpg" alt="">
-				    </div><!--//col-->
-				    <div class="col">
-					    <div class="row p-4 justify-content-center justify-content-md-between">
-						    <div class="primary-info col-auto">
-							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Member 1 name</h1>
-							    <div class="title mb-3">Member 1 role</div>
-								<a href="#<?php /*LINK_TO_MEMBER_1_PAGE*/ ?>" class="btn btn-secondary">See full profile</a>
-						    </div><!--//primary-info-->
-						    <div class="secondary-info col-auto mt-2">
-						    </div><!--//secondary-info-->
-					    </div><!--//row-->
-					    
-				    </div><!--//col-->
-			    </div><!--//row-->
-		    </header>
-		    
-		    <header class="resume-header mt-4 pt-4 pt-md-0">
-			    <div class="row">
-				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-				        <img class="picture" src="assets/images/profile.jpg" alt="">
-				    </div><!--//col-->
-				    <div class="col">
-					    <div class="row p-4 justify-content-center justify-content-md-between">
-						    <div class="primary-info col-auto">
-							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Member 2 name</h1>
-							    <div class="title mb-3">Member 2 role</div>
-								<a href="#<?php /*LINK_TO_MEMBER_2_PAGE*/ ?>" class="btn btn-secondary">See full profile</a>
-						    </div><!--//primary-info-->
-						    <div class="secondary-info col-auto mt-2">
-						    </div><!--//secondary-info-->
-					    </div><!--//row-->
-					    
-				    </div><!--//col-->
-			    </div><!--//row-->
-		    </header>
-		    
-		    <header class="resume-header mt-4 pt-4 pt-md-0">
-			    <div class="row">
-				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-				        <img class="picture" src="assets/images/profile.jpg" alt="">
-				    </div><!--//col-->
-				    <div class="col">
-					    <div class="row p-4 justify-content-center justify-content-md-between">
-						    <div class="primary-info col-auto">
-							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Member 3 name</h1>
-							    <div class="title mb-3">Member 3 role</div>
-								<a href="#<?php /*LINK_TO_MEMBER_3_PAGE*/ ?>" class="btn btn-secondary">See full profile</a>
-						    </div><!--//primary-info-->
-						    <div class="secondary-info col-auto mt-2">
-						    </div><!--//secondary-info-->
-					    </div><!--//row-->
-					    
-				    </div><!--//col-->
-			    </div><!--//row-->
-		    </header>
-		    
+			<h1 class="py-4 text-center"><?=$members['pageTitle'];?></h1>
+			<?php foreach ($members['memberInfo'] as $key => $userInfo){
+					echo '
+					<header class="resume-header pt-4 pt-md-0">
+						<div class="row">
+							<div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+								<img class="picture" src="' . $userInfo['headshot'] . '" alt="' . $userInfo['name'] . '">
+							</div><!--//col-->
+							<div class="col">
+								<div class="row p-4 justify-content-center justify-content-md-between">
+									<div class="primary-info col-auto">
+										<h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">' . $userInfo['name'] . '</h1>
+										<div class="title mb-3">' . $userInfo['role'] . '</div>
+
+										<a href="detail.php?index='. $userInfo['link'] .'" class="btn btn-secondary">See full profile</a>
+									</div><!--//primary-info-->
+									<div class="secondary-info col-auto mt-2">
+									</div><!--//secondary-info-->
+								</div><!--//row-->
+								
+							</div><!--//col-->
+						</div><!--//row-->
+					</header>';
+				}
+			?>
 	    </div>
     </article> 
 
     
     <footer class="footer text-center pt-2 pb-5">
 	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
-        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by Your names</small>
+        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by <?=$members['pageFooter'];?></small>
     </footer>
 
     
